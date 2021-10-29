f99131317a7fbb02b78088cda4f242bc604cf32b
Becky Hassler <beckyjhassler@gmail.com>
2021-10-28 20:00:28 -0400

    Create variables

---
+++ index.js
@@ -0,0 +1,70 @@
+// grab references to form, input, and #weather
+var formEl = document.querySelector('form')
+var inputEl = document.querySelector('input')
+var weatherEl = document.getElementById('weather')
+console.log(formEl, inputEl, weatherEl)
+
+// attach submit to form event
+// fetch weather data if there is a query
+// call render weather function
+formEl.onsubmit = function(e) {
+    e.preventDefault()
+    var query = inputEl.value.trim()
+    if (!query) return
+    console.log(query)
+    fetch('api.openweathermap.org/data/2.5/weather?q=' + query + '&units=imperial&appid=fe1a8ee3b8894757366a34ae524f361a')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(resultingJSON) {
+        renderWeather(resultingJSON)
+        inputEl.value = ""
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
+
+// function renderWeather(weatherObj) {
+//     weatherEl.innerHTML = ""
+//     if (weatherObj.Error) {
+//         weatherEl.textContent = weatherObj.Error
+//         return
+//     }
+    // // create title
+    // var title = document.createElement('h2')
+    // title.textContent = movieObj.Title + " (" + movieObj.Year + ")"
+    // movieEl.appendChild(title)
+
+    // // genre
+    // var genreRuntime = document.createElement('h4')
+    // genreRuntime.textContent = movieObj.Genre + " (" +  movieObj.Runtime + ")"
+    // // genreRuntime.style.fontStyle = 'italic'
+    // genreRuntime.style= 'font-style: italic;'
+    // movieEl.appendChild(genreRuntime)
+
+    // //poster
+    // var poster = document.createElement('img')
+    // poster.src = movieObj.Poster
+    // poster.alt = movieObj.Poster + " poster"
+    // movieEl.appendChild(poster)
+
+    // //plot
+    // var plot = document.createElement('p')
+    // plot.textContent = movieObj.Plot
+    // plot.style= 'font-style: italic;'
+    // movieEl.appendChild(plot)
+
+    // //ratings
+    // movieObj.Ratings.forEach(function(rating) {
+    //     var ratingEl = document.createElement('h5')
+    //     ratingEl.textContent = rating.Source + ": " + rating.Value
+    //     movieEl.appendChild(ratingEl)
+    // })
+    
+// }
+
+// renderMovie function
+// handle clearing previous movie
+// handle movie not found
+// render title, year, genre, runtime, poster, plot, and ratings
